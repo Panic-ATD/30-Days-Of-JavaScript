18d077db267086e0386b020790d8d7f2d81776b0
Aidan <97966679+Panic-ATD@users.noreply.github.com>
2024-09-30 07:11:26 +0800

    Create Filter Elements from Array.js

---
+++ Day 2/Filter Elements from Array.js
@@ -0,0 +1,12 @@
+/** 2634. Filter Elements from Array
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function (arr, fn) {
+    var _array = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) _array.push(arr[i]);
+    }
+    return _array;
+};
